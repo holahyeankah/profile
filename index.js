c71b916d60bbb282e9bff0a25139629c20c291ff
Olayinka <holahyeankah12@gmail.com>
2021-08-26 20:19:06 +0100

    changing env

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post("/api/post", (req, res)=>{
 
 
 
-const PORT= process.env.PORT || '3000'
+const PORT= process.env.PORT || '3001'
 app.listen(PORT,()=>{
 console.log(`Server is listening on port ${PORT}`)
 
